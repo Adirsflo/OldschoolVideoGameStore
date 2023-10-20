0b94b225761f146a15b80a71cbb0d486fd31873f
Adirsflo <rida.abdal@gmail.com>
2023-10-20 22:09:04 +0200

    Initial commit

---
+++ OldschoolVideoGameStore/MainWindow.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace OldschoolVideoGameStore
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+        private void btnLogin_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
